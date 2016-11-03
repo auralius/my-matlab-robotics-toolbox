92cb7acb02ffa4d634049c957039594da002c5e1
DESKTOP-7EHQ0Q0\auralius <manurung.auralius@gmail.com>
2016-11-03 10:33:47 +0100

    Added: damped least sqaure. Fixed: bug on joint limts.

--- cgr_ikine2.m
+++ cgr_ikine2.m
@@ -1,6 +1,7 @@
-function [q, k, err] = cgr_ikine1(r, p, treshold, max_iter)
-% https://groups.csail.mit.edu/drl/journal_club/papers/033005/buss-2004.pdf
-% See Equ. 7.
+function [q, k, err] = cgr_ikine2(r, p, lambda, treshold, max_iter)
+% Using damped least square method
+% http://math.ucsd.edu/~sbuss/ResearchWeb/ikmethods/iksurvey.pdf
+% See Equ. 11.
 % r is the sructure of the robot.
 % p (3x1) is the target cartesian position;
 %
@@ -25,7 +26,8 @@ while 1
     
     delta_x = p - x;
 
-    delta_q = pinv(jac)*delta_x;
+    K = (jac'*jac + lambda^2 .* eye(r.n))\jac';
+    delta_q = K*delta_x;
     q = q + delta_q;
     q = min(r.ub, max(q, r.lb)); % This is a saturation function.
     
