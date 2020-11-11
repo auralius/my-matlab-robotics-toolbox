40a763a4fe67284d416236b2e568bd68aaa14d3a
Auralius Manurung <auralius@users.noreply.github.com>
2020-11-11 13:39:43 +0700

    Added an example for 2 DOFs

---
+++ demo_2dofs.m
@@ -0,0 +1,32 @@
+%%
+clc;
+clear all;
+close all;
+
+%% Graphic
+g = ncgr_graphic();
+
+global N_DOFS;
+N_DOFS = 2;
+
+%% 2-R-planar robot DH-Parameters
+theta = [0 0];
+alpha = [0 0];
+offset = [0 0];
+d = [0 0];
+a = [0.5 0.5];
+type = ['r','r'];
+base = [0; 0; 0];
+
+planar_2r = cgr_create(theta, d, a, alpha, offset, type, base, ...
+    [pi/2; pi/2], [-pi/2; -pi/2;]);  % joint limts!
+planar_2r = cgr_self_update(planar_2r, [0; 0], base);
+g = ncgr_plot(g, planar_2r, [0 0 1], 1);
+
+for k = 0:0.1:pi
+    planar_2r = cgr_self_update(planar_2r, [0+k; 0+k], base);
+    g = ncgr_plot(g, planar_2r, [0 0 1], 1);
+    pause(0.5);
+end
+
+
